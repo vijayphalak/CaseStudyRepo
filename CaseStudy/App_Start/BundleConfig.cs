80d73c4b033993a486088d12f1e8a9191f1003d8
vijayphalak <vijay.phalak@kpit.com>
2018-12-20 19:47:30 +0530

    test commit

--- CaseStudy/App_Start/BundleConfig.cs
+++ CaseStudy/App_Start/BundleConfig.cs
@@ -20,6 +20,7 @@ namespace CaseStudy
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
