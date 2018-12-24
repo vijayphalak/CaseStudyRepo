72351c786b005629b35c24fbe9f1bfb58f92c27a
vijayphalak <vijay.phalak@kpit.com>
2018-12-24 18:17:22 +0530

    comm12

--- CaseStudy/App_Start/BundleConfig.cs
+++ CaseStudy/App_Start/BundleConfig.cs
@@ -11,8 +11,7 @@ namespace CaseStudy
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            
-            
+                        
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
